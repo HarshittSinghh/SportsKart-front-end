c67d5190756eaead00602373b41dbe9687b626f3
Harshit Kumar Singh <163038346+HarshittSinghh@users.noreply.github.com>
2024-03-22 03:45:40 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -8,14 +8,34 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB database
-mongoose.connect("mongodb://127.0.0.1:27017/employee");
+mongoose.connect("mongodb://127.0.0.1:27017/employee", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// Define route for creating
+// Define route for creating a new employee
 app.post("/register", (req, res) => {
   EmployeeModel.create(req.body)
     .then((employee) => res.json(employee))
     .catch((err) => res.json(err));
 });
+
+// Define route for logging in
+app.post("/Login", (req, res) => {
+  const { email, password } = req.body;
+  EmployeeModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("Success");
+      } else {
+        res.json("Password is incorrect");
+      }
+    } else {
+      res.json("User does not exist");
+    }
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is Running");
 });
