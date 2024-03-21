347e76d767c85f0fb80f11f868f2eb5266210076
Harshit Kumar Singh <163038346+HarshittSinghh@users.noreply.github.com>
2024-03-22 01:24:41 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const UserModel = require("./model/SportKart");
+const EmployeeModel = require("./model/employee");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB database
-mongoose.connect("mongodb://localhost:27017/SportsKart");
+mongoose.connect("mongodb://127.0.0.1:27017/employee");
 
-mongoose.connection.on("connected", () => {
-  console.log("Connected to MongoDB");
-});
-
-// Define route for creating users
-app.post("/users", (req, res) => {
-  User.create(req.body)
-    .then((user) => res.json(user))
+// Define route for creating
+app.post("/register", (req, res) => {
+  EmployeeModel.create(req.body)
+    .then((employee) => res.json(employee))
     .catch((err) => res.json(err));
 });
 app.listen(3000, () => {
