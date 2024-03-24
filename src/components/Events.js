6d1913313f9146dd23ca56cb64c64602d3da0315
Harshit Kumar Singh <163038346+HarshittSinghh@users.noreply.github.com>
2024-03-25 00:01:30 +0530

    Add files via upload

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,20 +1,18 @@
 import React from "react";
-import video from "./IMG/soon.mp4";
 import "./style.css";
-const soon = () => {
+
+function App() {
   return (
-    <>
-      <div className="coming-soon">
-        <video src={video} autoPlay muted loop></video>
-        <div className="coming-soon-text">
-          <h1>
-            STAY TUNED FOR UPDATES
-            <br />
-            COMING SOON!
-          </h1>
-        </div>
+    <div className="coming-soon">
+      <div className="text-part">
+        <h1>Coming Soon</h1>
+        <br />
+        <h3>For more exciting updates coming your way soon!</h3>
+        <br />
+        <p>Stay tuned!</p>
       </div>
-    </>
+    </div>
   );
-};
-export default soon;
+}
+
+export default App;
